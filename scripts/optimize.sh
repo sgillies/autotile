a341d0f59da30cd521833c5d61669611408af04f
Sean Gillies <sean.gillies@gmail.com>
2021-03-30 14:17:34 -0600

    Help the script pass on a fresh checkout
    @cisaacstern this was the only thing I need to change to get the project to run locally right out of the box. `|| true` is one way to do it. There could be others.

--- scripts/optimize.sh
+++ scripts/optimize.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-rm _notmatched.sqlite \
-&& rm _matched.sqlite \
+(rm _notmatched.sqlite || true) \
+&& (rm _matched.sqlite || true) \
 && echo Removed _notmached.sqlite and _matched.sqlite \
 && terracotta optimize-rasters --overwrite $TOA/*.tif -o $NOT_MATCHED \
 && terracotta optimize-rasters --overwrite $HISTOGRAM/*.tif -o $MATCHED \
